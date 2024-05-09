e46c66619de8869d3d2838ae3a61e49112a7eb02
986571880 <986571880@qq.com>
2024-05-09 19:57:20 +0800

    Increase targetSdkVersion to 33

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val applicationId = "com.xyoye.dandanplay"
     const val compileSdkVersion = 33
     const val minSdkVersion = 21
-    const val targetSdkVersion = 30
+    const val targetSdkVersion = 33
     const val versionCode = 58
     const val versionName = "4.2.0"
 }
